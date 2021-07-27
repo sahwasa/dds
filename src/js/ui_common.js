3fad6f788e8bdcd73a8e4d28017c343045a8262c
banmiseon <jjaq88777@hanmail.net>
2021-07-27 12:15:39 +0900

    지진유감신고하기 완료

---
+++ src/js/ui_common.js
@@ -0,0 +1,79 @@
+(function($){
+  $('.sub a').on({
+    focusin : function(){
+      $('.sub').removeClass('focusing');
+      $(this).parents('.sub').addClass('focusing');
+    },
+    focusout : function(){
+      // console.log($(this).parents('.sub').parent('li').eq())
+    }
+  })
+
+  // accordion
+  $( ".accordion > div" ).accordion({
+    header : "h3",
+    collapsible: false,
+    heightStyle: "content",
+    active:0
+  });
+
+  // datepicker
+  $.datepicker.setDefaults({
+    regional:["ko"],
+    buttonImageOnly: true,
+    showOn: "both",
+    buttonImage: "../img/btn_calendar.png",
+    changeMonth: true,
+    changeYear: true,
+    dateFormat:"yy-mm-dd"
+  });
+  $( "[dataformat='datepic']" ).datepicker({
+    buttonText: "날짜를 선택해주세요."
+  });
+  $( "[dataformat='from']" ).datepicker({
+    buttonText: "시작날짜를 선택해주세요.",
+    onClose: function( selectedDate ) {
+      var getName=$(this).attr('name');
+      $("input[name='"+ getName +"'].to").datepicker( "option", "minDate", selectedDate );
+    }
+  });
+  $( "[dataformat='to']" ).datepicker({
+    buttonText: "종료날짜를 선택해주세요.",
+    onClose: function( selectedDate ) {
+      var getName=$(this).attr('name');
+      $("input[name='"+ getName +"'].from").datepicker( "option", "maxDate", selectedDate );
+    }
+  });
+  // datetimepicker
+  function date_to_str(format){
+    var year = format.getFullYear(),
+        month = format.getMonth() + 1,
+        date = format.getDate(),
+        hour = format.getHours(),
+        min = format.getMinutes(),
+        sec = format.getSeconds();
+    if(month<10) month = '0' + month;
+    if(date<10) date = '0' + date;
+    hour = hour % 12;
+    hour = hour ? hour : 12;
+    if(hour<10) hour = '0' + hour;
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
+
+    return year + "-" + month + "-" + date + "  " + hour + ":" + min + ":" + sec;
+  }
+  $(".datetimepic").val( date_to_str(new Date()));
+  // use jqueryui-timepicker-addon
+  // https://trentrichardson.com/examples/timepicker/
+
+  $(".datetimepic").datetimepicker({
+    timeFormat: 'HH : mm : ss',
+    controlType: 'select',
+    oneLine: true
+  });
+  Date.prototype.addMinutes = function(minutes) {
+    this.setMinutes(this.getMinutes() + minutes);
+    return this;
+  };
+
+})(jQuery);
